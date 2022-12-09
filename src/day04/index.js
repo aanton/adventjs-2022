d8d3fa395d5c96cf3a7a2dae7fcf1fb78ca5e405
aanton <aanton@gmail.com>
2022-12-09 16:54:45 +0100

    Fix semantic in day 4 challenge

--- src/day04/index.js
+++ src/day04/index.js
@@ -6,12 +6,12 @@ function fitsInOneBox(boxes) {
   const fitsInPreviousBox = (
     /** @type {{ l: any; w: any; h: any; }} */ previous,
     /** @type {{ l: any; w: any; h: any; }} */ next
-  ) => previous.l >= next.l || previous.w >= next.w || previous.h >= next.h
+  ) => previous.l < next.l && previous.w < next.w && previous.h < next.h
 
   boxes.sort((a, b) => a.l - b.l)
 
   for (let i = 0; i < boxes.length - 1; i++) {
-    if (fitsInPreviousBox(boxes[i], boxes[i + 1])) return false
+    if (!fitsInPreviousBox(boxes[i], boxes[i + 1])) return false
   }
 
   return true
