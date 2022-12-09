c924d683ebb8ff8434b39eb0ccae88f7b6319a69
aanton <aanton@gmail.com>
2022-12-09 17:12:32 +0100

    Improve day 6 challenge

--- src/day06/index.js
+++ src/day06/index.js
@@ -3,13 +3,13 @@
  * @returns {string}
  */
 function createCube(size) {
-  const top = [],
-    bottom = []
+  const top = Array.from({length: size}, (_, i) => i + 1)
+    // i: 1, 2, 3, ..., size
+    .map(i => ' '.repeat(size - i) + '/\\'.repeat(i) + '_\\'.repeat(size))
 
-  for (let i = 1; i <= size; i++) {
-    top.push(' '.repeat(size - i) + '/\\'.repeat(i) + '_\\'.repeat(size))
-    bottom.unshift(' '.repeat(size - i) + '\\/'.repeat(i) + '_/'.repeat(size))
-  }
+  const bottom = Array.from({length: size}, (_, i) => size - i)
+    // i: size, ..., 3, 2, 1
+    .map(i => ' '.repeat(size - i) + '\\/'.repeat(i) + '_/'.repeat(size))
 
   return [...top, ...bottom].join('\n')
 }
