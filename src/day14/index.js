ab08e41ae1568d61708d776d3ae37d4ebe5ec90e
aanton <aanton@gmail.com>
2022-12-17 11:02:13 +0100

    Optimize solution for day 14 challenge

--- src/day14/index.js
+++ src/day14/index.js
@@ -24,15 +24,17 @@ function getOptimalPath(path) {
  * @returns number
  */
 function getOptimalPathOptimized(path) {
-  const memory = path.pop()
+  // const memory = path.pop()
+  // for (let i = path.length - 1; i >= 0; i--) {
+  //   for (let j = 0; j < path[i].length; j++) {
+  //     memory[j] = path[i][j] + Math.min(memory[j], memory[j + 1])
+  //   }
+  // }
+  // return memory[0]
 
-  for (let i = path.length - 1; i >= 0; i--) {
-    for (let j = 0; j < path[i].length; j++) {
-      memory[j] = Math.min(memory[j], memory[j + 1]) + path[i][j]
-    }
-  }
-
-  return memory[0]
+  return path.reduceRight((previous, current) =>
+    current.map((v, i) => v + Math.min(previous[i], previous[i + 1]))
+  )[0]
 }
 
 export {getOptimalPath, getOptimalPathOptimized}
